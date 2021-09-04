184b073f3529a71a4915fb9c4e4ad0305fc185fb
Silvio Silva <smsilva@gmail.com>
2021-09-04 16:25:13 -0300

    Refatorando

--- src/outputs.tf
+++ src/outputs.tf
@@ -2,8 +2,24 @@ output "platform_resource_group_name" {
   value = azurerm_resource_group.foundation.name
 }
 
+output "platform_key_vault_id" {
+  value = module.vault.instance.id
+}
+
+output "platform_key_vault_name" {
+  value = module.vault.instance.name
+}
+
+output "platform_storage_account_id" {
+  value = module.backend_storage.storage_account.id
+}
+
 output "platform_storage_account_name" {
   value = module.backend_storage.storage_account.name
+}
+
+output "platform_storage_account_container_id" {
+  value = module.backend_storage.storage_container.id
 }
 
 output "platform_storage_account_container_name" {
