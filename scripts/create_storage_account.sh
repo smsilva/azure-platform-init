6fb15f6cf1662d0fc4b1af9ceffe3231222a18bd
Silvio Silva <smsilva@gmail.com>
2021-12-06 21:32:44 -0300

    Refatorando

--- scripts/create_storage_account.sh
+++ scripts/create_storage_account.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+SUBSCRIPTION_ID=$(az account show \
+  --output tsv \
+  --query id)
+
 AZURE_RESOURCE_GROUP_NAME="wasp-foundation"
-AZURE_STORAGE_ACCOUNT_NAME="waspfoundationb8873609"
+AZURE_STORAGE_ACCOUNT_NAME="waspfoundation$(awk -F '-' '{ print $1 }' <<< "${SUBSCRIPTION_ID}")"
 AZURE_LOCATION="centralus"
+
+echo "AZURE_RESOURCE_GROUP_NAME..: ${AZURE_RESOURCE_GROUP_NAME}"
+echo "AZURE_STORAGE_ACCOUNT_NAME.: ${AZURE_STORAGE_ACCOUNT_NAME}"
+echo "AZURE_LOCATION.............: ${AZURE_LOCATION}"
 
 az group create \
   --resource-group "${AZURE_RESOURCE_GROUP_NAME}" \
